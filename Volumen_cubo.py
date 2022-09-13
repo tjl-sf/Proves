e090f89eb24773c545ffcfad4e60e7d3f187be28
tjl05 <40433488+tjl05@users.noreply.github.com>
2022-09-13 15:49:19 +0200

    Hemos añadido el script que calcula el volumen

---
+++ Volumen_cubo.py
@@ -0,0 +1,12 @@
+# Python Program to Calculate the Volume and Surface Area of Cuboid
+
+# Take the input from the User
+length = float(input("Enter the Length of a Cuboid: "))
+width = float(input("Enter the Width of a Cuboid: "))
+height = float(input("Enter the Height of a Cuboid: "))
+
+# Calculate the Volume
+Volume = length * width * height
+
+# Print the Output
+print("The Volume of a Cuboid = %.2f" %Volume);
