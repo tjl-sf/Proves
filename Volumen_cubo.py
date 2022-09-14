f2bb42b21482681e6ec64b85da762e6d9584397a
tjl05 <40433488+tjl05@users.noreply.github.com>
2022-09-14 13:16:23 +0200

    Control tipo

--- Volumen_cubo.py
+++ Volumen_cubo.py
@@ -5,6 +5,11 @@ length = float(input("Enter the Length of a Cuboid: "))
 width = float(input("Enter the Width of a Cuboid: "))
 height = float(input("Enter the Height of a Cuboid: "))
 
+if length < 0:
+    raise ValueError("No tiene sentido introducir longitud negativa")
+
+if type(length) not in [int, float]:
+    raise ValueError("La longitud tiene que ser real o entero")
 # Calculate the Volume
 Volume = length * width * height;
 
